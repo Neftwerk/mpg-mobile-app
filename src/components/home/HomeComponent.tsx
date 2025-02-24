6477518be06a2e83782e5b81e29c551887cc253b
ElectroCERP <carloscer27r@gmail.com>
2025-02-24 17:50:08 -0300

    Update HomeComponent to render HomePageButtonsComponent

--- src/components/home/HomeComponent.tsx
+++ src/components/home/HomeComponent.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { ScrollView, Text, View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+
+import { HomePageButtonsComponent } from './HomePageButtons/HomePageButtonsComponent';
 
 export const HomeComponent = () => {
-	const [greeting, setGreeting] = React.useState('');
-	const data = () => {
-		fetch(`${process.env.EXPO_PUBLIC_API_URL}/hello`)
-			.then((res) => {
-				return res;
-			})
-			.then((res) => res.json())
-			.then((res) => setGreeting(res.greeting))
-			.catch((err) => console.log(err))
-			.finally(() => console.log(greeting));
-	};
-
-	React.useEffect(() => {
-		data();
-	}, []);
-
 	return (
 		<ScrollView>
-			<View testID="welcome-view">
-				<Text className="p-5 font-bold">{greeting}</Text>
+			<View testID="HomeScreen">
+				<HomePageButtonsComponent />
 			</View>
 		</ScrollView>
 	);
