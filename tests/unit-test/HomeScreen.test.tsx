4b508fa0217342753a12819ed029018d088e3d02
ElectroCERP <carloscer27r@gmail.com>
2025-02-24 17:47:19 -0300

    Update HomeScreen unit tests

--- tests/unit-test/HomeScreen.test.tsx
+++ tests/unit-test/HomeScreen.test.tsx
@@ -1,19 +1,22 @@
-import { render, waitFor } from '@testing-library/react-native';
+import { render } from '@testing-library/react-native';
 import React from 'react';
 
 import { HomeComponent } from '@/components/home/HomeComponent';
 
 describe('HomeScreen', () => {
-	test('Should home screen match snapshot', async () => {
-		const tree = render(<HomeComponent />).toJSON();
-		await waitFor(() => expect(tree).toMatchSnapshot());
-	});
-
-	test('Text renders correctly on HomeScreen', async () => {
+	test('Should render home page buttons', async () => {
 		const { findByText } = render(<HomeComponent />);
 
-		const greetingText = await findByText('Welcome!');
+		const forumButton = await findByText('Forum');
+		const galleryButton = await findByText('Gallery');
+		const discoverButton = await findByText('Discover');
+		const walletButton = await findByText('Wallet');
+		const profileButton = await findByText('Profile');
 
-		expect(greetingText).toBeTruthy();
+		expect(forumButton).toBeTruthy();
+		expect(galleryButton).toBeTruthy();
+		expect(discoverButton).toBeTruthy();
+		expect(walletButton).toBeTruthy();
+		expect(profileButton).toBeTruthy();
 	});
 });
