21ea7f4f070f5055a7651bf793fc71d12500a823
ElectroCERP <carloscer27r@gmail.com>
2025-02-24 17:34:36 -0300

    Remove Expo Asset import from BackgoundImageWrapper since no longer needed

--- src/components/BackgroundWrapper/BackgroundWrapper.tsx
+++ src/components/BackgroundWrapper/BackgroundWrapper.tsx
@@ -1,4 +1,3 @@
-import { Asset } from 'expo-asset';
 import React from 'react';
 import { ImageBackground, View } from 'react-native';
 
@@ -6,16 +5,12 @@ interface Props {
 	children: React.ReactNode;
 }
 
-const backgroundPattern = Asset.fromModule(
-	require('../../assets/png/background-pattern.png'),
-);
-
 const BackgroundWrapper: React.FC<Props> = ({ children }) => {
 	return (
 		<View className="flex-1 absolute w-full h-full" testID="BackgroundWrapper">
 			<View className="flex-1 bg-[#EEE9E7] absolute top-0 left-0 right-0 bottom-0" />
 			<ImageBackground
-				source={{ uri: backgroundPattern.uri }}
+				source={require('../../assets/png/background-pattern.png')}
 				className="flex-1 w-full h-full"
 				resizeMode="cover"
 			>
