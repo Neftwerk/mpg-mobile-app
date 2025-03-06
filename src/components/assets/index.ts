25af78c4276bd52fe55f6b5655ca78246bfad12a
ElectroCERP <carloscer27r@gmail.com>
2025-03-06 18:19:32 -0300

    Create a barrel file to export all the needed png from a single directory

---
+++ src/components/assets/index.ts
@@ -0,0 +1,6 @@
+export { default as arrowBackIcon } from '../../assets/arrowBackIcon.png';
+export { default as menuIcon } from '../../assets/hamburgerMenuClosedIcon.png';
+export { default as profileIcon } from '../../assets/profileIcon.png';
+export { default as galleryIcon } from '../../assets/galleryIcon.png';
+export { default as walletIcon } from '../../assets/walletIcon.png';
+export { default as backgroundPattern } from '../../assets/background-pattern.png';
