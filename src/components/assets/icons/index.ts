3fbc1587182a81245cb20951d062cfb249cd3e0e
ElectroCERP <carloscer27r@gmail.com>
2025-03-07 16:55:36 -0300

    Create a barrel file to export all the icon components

---
+++ src/components/assets/icons/index.ts
@@ -0,0 +1,7 @@
+export { ArrowBackIcon } from './ArrowBackIcon';
+export { MenuIcon } from './MenuIcon';
+export { ProfileIcon } from './ProfileIcon';
+export { WalletIcon } from './WalletIcon';
+export { GalleryIcon } from './GalleryIcon';
+export { ForumIcon } from './ForumIcon';
+export { DiscoverIcon } from './DiscoverIcon';
