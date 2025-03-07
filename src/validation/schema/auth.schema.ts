5edcf8def815298f356bdeee1df8384ad5bdb10b
ElectroCERP <carloscer27r@gmail.com>
2025-03-07 16:51:58 -0300

    Move Auth schema validation error messages to a constant

--- src/validation/schema/auth.schema.ts
+++ src/validation/schema/auth.schema.ts
@@ -1,45 +1,63 @@
 import * as Yup from 'yup';
 
+import { AUTH_ERROR_MESSAGES } from '@/constants/errors';
+
+const PASSWORD_REGEX =
+	/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,24}$/;
+
 export const loginSchema = Yup.object().shape({
-	username: Yup.string().email('Invalid email').required('Email is required'),
-	password: Yup.string()
-		.required('Password is required')
-		.min(8, 'Password must be at least 8 characters')
-		.max(24, 'Password must be less than 24 characters'),
+	username: Yup.string()
+		.email(AUTH_ERROR_MESSAGES.EMAIL_INVALID_ERROR_MESSAGE)
+		.required(AUTH_ERROR_MESSAGES.EMAIL_REQUIRED_ERROR_MESSAGE),
+	password: Yup.string().required(
+		AUTH_ERROR_MESSAGES.PASSWORD_REQUIRED_ERROR_MESSAGE,
+	),
 });
 
 export const signUpSchema = Yup.object().shape({
-	username: Yup.string().email('Invalid email').required('Email is required'),
+	username: Yup.string()
+		.email(AUTH_ERROR_MESSAGES.EMAIL_INVALID_ERROR_MESSAGE)
+		.required(AUTH_ERROR_MESSAGES.EMAIL_REQUIRED_ERROR_MESSAGE),
 	password: Yup.string()
-		.required('Password is required')
-		.min(8, 'Password must be at least 8 characters')
-		.max(24, 'Password must be at most 24 characters')
+		.required(AUTH_ERROR_MESSAGES.PASSWORD_REQUIRED_ERROR_MESSAGE)
+		.min(8, AUTH_ERROR_MESSAGES.PASSWORD_MIN_LENGTH_ERROR_MESSAGE)
+		.max(24, AUTH_ERROR_MESSAGES.PASSWORD_MAX_LENGTH_ERROR_MESSAGE)
 		.matches(
-			/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&.])[A-Za-z\d@$!%*?&.]{8,24}$/,
-			'Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character (@, $, !, %, *, ?, &, .)',
+			PASSWORD_REGEX,
+			AUTH_ERROR_MESSAGES.PASSWORD_REGEX_MATCH_ERROR_MESSAGE,
 		),
-	name: Yup.string().required('First name is required'),
-	surname: Yup.string().required('Last name is required'),
+	name: Yup.string().required(AUTH_ERROR_MESSAGES.NAME_REQUIRED_ERROR_MESSAGE),
+	surname: Yup.string().required(
+		AUTH_ERROR_MESSAGES.SURNAME_REQUIRED_ERROR_MESSAGE,
+	),
 	biography: Yup.string()
 		.optional()
-		.max(255, 'Biography must be less than 255 characters'),
+		.max(255, AUTH_ERROR_MESSAGES.BIOGRAPHY_MAX_LENGTH_ERROR_MESSAGE),
 });
 
 export const forgotPasswordSchema = Yup.object().shape({
-	username: Yup.string().email('Invalid email').required('Email is required'),
+	username: Yup.string()
+		.email(AUTH_ERROR_MESSAGES.EMAIL_INVALID_ERROR_MESSAGE)
+		.required(AUTH_ERROR_MESSAGES.EMAIL_REQUIRED_ERROR_MESSAGE),
 });
 
 export const confirmPasswordSchema = Yup.object().shape({
 	code: Yup.string()
-		.length(6, 'Code must be 6 characters max')
-		.required('Code is required'),
-	username: Yup.string().email('Invalid email').required('Email is required'),
-	newPassword: Yup.string().required('Password is required'),
+		.length(6, AUTH_ERROR_MESSAGES.CODE_LENGTH_ERROR_MESSAGE)
+		.required(AUTH_ERROR_MESSAGES.CODE_REQUIRED_ERROR_MESSAGE),
+	username: Yup.string()
+		.email(AUTH_ERROR_MESSAGES.EMAIL_INVALID_ERROR_MESSAGE)
+		.required(AUTH_ERROR_MESSAGES.EMAIL_REQUIRED_ERROR_MESSAGE),
+	newPassword: Yup.string().required(
+		AUTH_ERROR_MESSAGES.PASSWORD_REQUIRED_ERROR_MESSAGE,
+	),
 });
 
 export const confirmUserSchema = Yup.object().shape({
 	code: Yup.string()
-		.length(6, 'Code must be 6 characters max')
-		.required('Code is required'),
-	username: Yup.string().email('Invalid email').required('Email is required'),
+		.length(6, AUTH_ERROR_MESSAGES.CODE_LENGTH_ERROR_MESSAGE)
+		.required(AUTH_ERROR_MESSAGES.CODE_REQUIRED_ERROR_MESSAGE),
+	username: Yup.string()
+		.email(AUTH_ERROR_MESSAGES.EMAIL_INVALID_ERROR_MESSAGE)
+		.required(AUTH_ERROR_MESSAGES.EMAIL_REQUIRED_ERROR_MESSAGE),
 });
