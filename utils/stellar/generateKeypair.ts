9cdf8e4fac9d4d79320c52e413cb79e156409320
ElectroCERP <carloscer27r@gmail.com>
2025-03-12 16:16:11 -0300

    Update generateKeypair util to use only Stellar

--- utils/stellar/generateKeypair.ts
+++ utils/stellar/generateKeypair.ts
@@ -1,9 +1,5 @@
 import { Keypair } from '@stellar/stellar-sdk';
-import { Buffer } from 'buffer';
-import * as Crypto from 'expo-crypto';
 
 export const generateKeypair = () => {
-	const randomBytes = Crypto.getRandomBytes(32);
-	const keypair = Keypair.fromRawEd25519Seed(Buffer.from(randomBytes));
-	return keypair;
+	return Keypair.random();
 };
