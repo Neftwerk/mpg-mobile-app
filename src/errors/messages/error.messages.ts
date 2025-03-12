e9359b4a1132f512487fa4fbcf1aff9d44248f3b
ElectroCERP <carloscer27r@gmail.com>
2025-03-12 11:03:47 -0300

    Create ERROR_MESSAGES const to show error messages when saving the user wallet secret key

---
+++ src/errors/messages/error.messages.ts
@@ -0,0 +1,5 @@
+export const ERROR_MESSAGES = {
+	GETTING_SECRET_KEY: 'Error getting secret key:',
+	STORING_KEYPAIR: 'Error storing keypair',
+	GEN_DEVICE_KEYPAIR: 'Error generating device keypair',
+};
