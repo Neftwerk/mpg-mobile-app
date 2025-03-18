0cbf1804d26271500c17debb5257f13189439660
ElectroCERP <carloscer27r@gmail.com>
2025-03-18 14:59:47 -0300

    Add more Error messages to Error constant

--- src/errors/messages/error.messages.ts
+++ src/errors/messages/error.messages.ts
@@ -2,4 +2,15 @@ export const ERROR_MESSAGES = {
 	GETTING_SECRET_KEY: 'Error getting secret key:',
 	STORING_KEYPAIR: 'Error storing keypair',
 	GEN_DEVICE_KEYPAIR: 'Error generating device keypair',
+	GENERATING_RECOVERY_ACCOUNTS:
+		'Error generating recovery accounts. Please try again.',
+	SIGNING_CHALLENGES: 'Error signing challenges:',
+	SUBMITTING_TRANSACTION: 'Error submitting transaction:',
+	GENERATING_RECOVERY_TOKENS: 'Error generating recovery tokens:',
+	CONFIGURING_RECOVERY: 'Error configuring recovery:',
+	SAVING_SIGNER: 'Error saving signers',
+	USER_NOT_FOUND_BEFORE_RECOVERY: 'User not found before recovering account',
+	RECOVERY_CODE_MAX_LENGTH_ERROR_MESSAGE:
+		'Recovery code must be 5 characters long',
+	RECOVERY_CODE_REQUIRED_ERROR_MESSAGE: 'Recovery code is required',
 };
